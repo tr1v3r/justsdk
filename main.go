8b399cb6eb402d67a8a4e3ba0402c6def2bed3ee
r1v3r <churiver@outlook.com>
2023-07-12 16:18:16 +0800

    feat: add new func GetAvg

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 // Package sdk provides ...
 package sdk
 
+// GetSum ...
 func GetSum(a int, b int) int {
 	return a + b
+}
+
+// GetAvg ...
+func GetAvg(nums ...int) float64 {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+
+	return float64(sum) / float64(len(nums))
 }
